0a1dd01af3fc1bcadc2456c715de3811ad316c3a
sfnvm <trungpt.dev@gmail.com>
2022-04-20 10:42:50 +0700

    change db to h2

--- src/test/java/com/example/hazelcastcache/HazelCastCacheApplicationTests.java
+++ src/test/java/com/example/hazelcastcache/HazelCastCacheApplicationTests.java
@@ -5,9 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HazelCastCacheApplicationTests {
-
-  @Test
-  void contextLoads() {
-  }
-
+    @Test
+    void contextLoads() {
+    }
 }
